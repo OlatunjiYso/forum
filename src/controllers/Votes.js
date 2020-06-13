eaa57153cda5ce90cd180f14d6f9b59e48fa9365
Olatunji <olatunjiyso@gmail.com>
2020-06-13 14:12:31 +0000

    refactor votes controllers responses to be consistent

--- src/controllers/Votes.js
+++ src/controllers/Votes.js
@@ -15,13 +15,13 @@ class VotesController {
             const { type, questionId } = req.body;
             const userId = req.user.id;
             let question = await Question.findOne({ _id: questionId });
-            if(!question) return res.status(404).json({msg: 'found no question with specified id'});
+            if(!question) return res.status(404).json({success: false, msg: 'found no question with specified id'});
             const votesCount = question.votes;
             const previousVote = await Vote.findOne({ question: questionId, voter: userId });
             if (previousVote) {
                 if (previousVote.type === type) {
                     return res.status(403)
-                        .json({ msg: `You have already ${type}d this question`, question})
+                        .json({ success: false, msg: `You have already ${type}d this question`, question})
                 };
                 await previousVote.updateOne({ type });
                 previousVote.save();
@@ -29,17 +29,18 @@ class VotesController {
                 question.votes = votesCount + voteValue;
                 question.save();
                 return res.status(200)
-                    .json({ msg: 'your vote has been updated', question })
+                    .json({ success: true, msg: 'your vote has been updated', question })
             }
             await Vote.create({ question: questionId, voter: userId, type });
             const voteValue = (type === 'upvote') ? 1 : -1;
             question.votes = votesCount + voteValue;
             question.save();
             return res.status(200)
-                .json({ msg: 'your vote has been added', question })
+                .json({ success: true, msg: 'your vote has been added', question })
         } catch (err) {
             return res.status(500)
                 .json({
+                    success: false,
                     msg: 'an internal server error occured while voting',
                     errMessage: err.message
                 })
